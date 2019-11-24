ae898ddf0df53056a9963749e695e4024b843d39
Maurer <dasmaurer@gmail.com>
2019-11-24 18:38:24 -0300

    últmos ajustes HTML & JS

--- js/juego.js
+++ js/juego.js
@@ -176,176 +176,144 @@ function moverEnDireccion(direccion) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //////////////////////////////////////////////////////////
-////////A CONTINUACIÓN FUNCIONES YA IMPLEMENTADAS.////////
-/////////NO TOCAR A MENOS QUE SEPAS LO QUE HACES//////////
-//////////////////////////////////////////////////////////
+    ////////A CONTINUACIÓN FUNCIONES YA IMPLEMENTADAS.////////
+    /////////NO TOCAR A MENOS QUE SEPAS LO QUE HACES//////////
+    //////////////////////////////////////////////////////////
 
-/* Las funciones y variables que se encuentran a continuación ya están implementadas.
-No hace falta que entiendas exactamente que es lo que hacen, ya que contienen
-temas aún no vistos. De todas formas, cada una de ellas tiene un comentario
-para que sepas que se está haciendo a grandes rasgos. NO LAS MODIFIQUES a menos que
-entiendas perfectamente lo que estás haciendo! */
+    /* Las funciones y variables que se encuentran a continuación ya están implementadas.
+    No hace falta que entiendas exactamente que es lo que hacen, ya que contienen
+    temas aún no vistos. De todas formas, cada una de ellas tiene un comentario
+    para que sepas que se está haciendo a grandes rasgos. NO LAS MODIFIQUES a menos que
+    entiendas perfectamente lo que estás haciendo! */
 
-/* codigosDireccion es un objeto que te permite reemplazar
-el uso de números confusos en tu código. Para referirte a la dir
-izquierda, en vez de usar el número 37, ahora podés usar:
-codigosDireccion.IZQUIERDA. Esto facilita mucho la lectura del código. */
-var codigosDireccion = {
-    IZQUIERDA: 37,
-    ARRIBA: 38,
-    DERECHA: 39,
-    ABAJO: 40
-}
-
-/* Funcion que realiza el intercambio logico (en la grilla) y ademas actualiza
-el intercambio en la pantalla (DOM). Para que funcione debera estar implementada
-la funcion intercambiarPosicionesGrilla() */
-function intercambiarPosiciones(fila1, columna1, fila2, columna2) {
-  // Intercambio posiciones en la grilla
-  var pieza1 = grilla[fila1][columna1];
-  var pieza2 = grilla[fila2][columna2];
-
-  intercambiarPosicionesGrilla(fila1, columna1, fila2, columna2);
-  intercambiarPosicionesDOM('pieza' + pieza1, 'pieza' + pieza2);
-
-}
-
-/* Intercambio de posiciones de los elementos del DOM que representan
-las fichas en la pantalla */
-
-function intercambiarPosicionesDOM(idPieza1, idPieza2) {
-  // Intercambio posiciones en el DOM
-  var elementoPieza1 = document.getElementById(idPieza1);
-  var elementoPieza2 = document.getElementById(idPieza2);
-
-  var padre = elementoPieza1.parentNode;
-
-  var clonElemento1 = elementoPieza1.cloneNode(true);
-  var clonElemento2 = elementoPieza2.cloneNode(true);
-
-  padre.replaceChild(clonElemento1, elementoPieza2);
-  padre.replaceChild(clonElemento2, elementoPieza1);
-}
-
-/* Actualiza la representación visual del último movimiento 
-en la pantalla, representado con una flecha. */
-function actualizarUltimoMovimiento(direccion) {
-  ultimoMov = document.getElementById('flecha');
-  switch (direccion) {
-    case codigosDireccion.ARRIBA:
-      ultimoMov.textContent = '↑';
-      break;
-    case codigosDireccion.ABAJO:
-      ultimoMov.textContent = '↓';
-      break;
-    case codigosDireccion.DERECHA:
-      ultimoMov.textContent = '→';
-      break;
-    case codigosDireccion.IZQUIERDA:
-      ultimoMov.textContent = '←';
-      break;
+    /* codigosDireccion es un objeto que te permite reemplazar
+    el uso de números confusos en tu código. Para referirte a la dir
+    izquierda, en vez de usar el número 37, ahora podés usar:
+    codigosDireccion.IZQUIERDA. Esto facilita mucho la lectura del código. */
+    var codigosDireccion = {
+      IZQUIERDA: 37,
+      ARRIBA: 38,
+      DERECHA: 39,
+      ABAJO: 40
   }
-}
 
-/* Esta función permite agregar una instrucción a la lista
-con idLista. Se crea un elemento li dinámicamente con el texto 
-pasado con el parámetro "instrucción". */
-function mostrarInstruccionEnLista(instruccion, idLista) {
-  var ul = document.getElementById(idLista);
-  var li = document.createElement("li");
-  li.textContent = instruccion;
-  ul.appendChild(li);
-}
+  /* Funcion que realiza el intercambio logico (en la grilla) y ademas actualiza
+  el intercambio en la pantalla (DOM). Para que funcione debera estar implementada
+  la funcion intercambiarPosicionesGrilla() */
+  function intercambiarPosiciones(fila1, columna1, fila2, columna2) {
+    // Intercambio posiciones en la grilla
+    var pieza1 = grilla[fila1][columna1];
+    var pieza2 = grilla[fila2][columna2];
 
-/* Función que mezcla las piezas del tablero una cantidad de veces dada.
-Se calcula una posición aleatoria y se mueve en esa dirección. De esta forma
-se mezclará todo el tablero. */
+    intercambiarPosicionesGrilla(fila1, columna1, fila2, columna2);
+    intercambiarPosicionesDOM('pieza' + pieza1, 'pieza' + pieza2);
 
-function mezclarPiezas(veces) {
-  if (veces <= 0) {
-    return;
   }
+
+  /* Intercambio de posiciones de los elementos del DOM que representan
+  las fichas en la pantalla */
+
+  function intercambiarPosicionesDOM(idPieza1, idPieza2) {
+    // Intercambio posiciones en el DOM
   
-  var direcciones = [codigosDireccion.ABAJO, codigosDireccion.ARRIBA,
-      codigosDireccion.DERECHA, codigosDireccion.IZQUIERDA
-    ];
+    var elementoPieza1 = document.getElementById(idPieza1);
+    var elementoPieza2 = document.getElementById(idPieza2);
 
-  var direccion = direcciones[Math.floor(Math.random() * direcciones.length)];
-  moverEnDireccion(direccion);
+    var padre = elementoPieza1.parentNode;
 
-  setTimeout(function() {
-      mezclarPiezas(veces - 1);
-    }, 100);
-}
+    var clonElemento1 = elementoPieza1.cloneNode(true);
+    var clonElemento2 = elementoPieza2.cloneNode(true);
 
-/* capturarTeclas: Esta función captura las teclas presionadas por el usuario. Javascript
-permite detectar eventos, por ejemplo, cuando una tecla es presionada y en 
-base a eso hacer algo. No es necesario que entiendas como funciona esto ahora, 
-en el futuro ya lo vas a aprender. Por ahora, sólo hay que entender que cuando
-se toca una tecla se hace algo en respuesta, en este caso, un movimiento */
-function capturarTeclas() {
-  document.body.onkeydown = (function(evento) {
-    if (evento.which === codigosDireccion.ABAJO ||
-      evento.which === codigosDireccion.ARRIBA ||
-      evento.which === codigosDireccion.DERECHA ||
-      evento.which === codigosDireccion.IZQUIERDA) {
+    padre.replaceChild(clonElemento1, elementoPieza2);
+    padre.replaceChild(clonElemento2, elementoPieza1);
+  }
 
-      moverEnDireccion(evento.which);
+  /* Actualiza la representación visual del último movimiento 
+  en la pantalla, representado con una flecha. */
+  function actualizarUltimoMovimiento(direccion) {
+    ultimoMov = document.getElementById('flecha');
+    switch (direccion) {
+      case codigosDireccion.ARRIBA:
+        ultimoMov.textContent = '↑';
+        break;
+      case codigosDireccion.ABAJO:
+        ultimoMov.textContent = '↓';
+        break;
+      case codigosDireccion.DERECHA:
+        ultimoMov.textContent = '→';
+        break;
+      case codigosDireccion.IZQUIERDA:
+        ultimoMov.textContent = '←';
+        break;
+    }
+  }
 
-        var gano = chequearSiGano();
-        if (gano) {
-          setTimeout(function() {
-              mostrarCartelGanador();
-              }, 500);
-            }
-            evento.preventDefault();
-        }
-    })
-}
+  /* Esta función permite agregar una instrucción a la lista
+  con idLista. Se crea un elemento li dinámicamente con el texto 
+  pasado con el parámetro "instrucción". */
+  function mostrarInstruccionEnLista(instruccion, idLista) {
+    var ul = document.getElementById(idLista);
+    var li = document.createElement("li");
+    li.textContent = instruccion;
+    ul.appendChild(li);
+  }
 
-/* Se inicia el rompecabezas mezclando las piezas 60 veces 
-y ejecutando la función para que se capturen las teclas que 
-presiona el usuario */
-function iniciar() {
-    mostrarInstrucciones(instrucciones);
-    mezclarPiezas(30);
-    capturarTeclas();
-}
+  /* Función que mezcla las piezas del tablero una cantidad de veces dada.
+  Se calcula una posición aleatoria y se mueve en esa dirección. De esta forma
+  se mezclará todo el tablero. */
 
-// Ejecutamos la función iniciar
-iniciar();
+  function mezclarPiezas(veces) {
+    if (veces <= 0) {
+      return;
+    }
+    
+    var direcciones = [codigosDireccion.ABAJO, codigosDireccion.ARRIBA,
+        codigosDireccion.DERECHA, codigosDireccion.IZQUIERDA
+      ];
+
+    var direccion = direcciones[Math.floor(Math.random() * direcciones.length)];
+    moverEnDireccion(direccion);
+
+    setTimeout(function() {
+        mezclarPiezas(veces - 1);
+      }, 100);
+  }
+
+  /* capturarTeclas: Esta función captura las teclas presionadas por el usuario. Javascript
+  permite detectar eventos, por ejemplo, cuando una tecla es presionada y en 
+  base a eso hacer algo. No es necesario que entiendas como funciona esto ahora, 
+  en el futuro ya lo vas a aprender. Por ahora, sólo hay que entender que cuando
+  se toca una tecla se hace algo en respuesta, en este caso, un movimiento */
+  function capturarTeclas() {
+    document.body.onkeydown = (function(evento) {
+      if (evento.which === codigosDireccion.ABAJO ||
+        evento.which === codigosDireccion.ARRIBA ||
+        evento.which === codigosDireccion.DERECHA ||
+        evento.which === codigosDireccion.IZQUIERDA) {
+
+        moverEnDireccion(evento.which);
+
+          var gano = chequearSiGano();
+          if (gano) {
+            setTimeout(function() {
+                mostrarCartelGanador();
+                }, 500);
+              }
+              evento.preventDefault();
+          }
+      })
+  }
+
+  /* Se inicia el rompecabezas mezclando las piezas 60 veces 
+  y ejecutando la función para que se capturen las teclas que 
+  presiona el usuario */
+  function iniciar() {
+      
+      mostrarInstrucciones(instrucciones);
+      mezclarPiezas(10);
+      capturarTeclas();
+  }
+
+  // Ejecutamos la función iniciar
+  iniciar();
